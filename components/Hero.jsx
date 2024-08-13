ecd1a8d8b4626d13351a34399a944d1f71fa3501
KevinBermudezC <kevinbermudezc@gmail.com>
2024-08-12 23:13:17 -0500

    "Updated Hero and Highlights components with new GSAP animations and added UI elements"

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -32,7 +32,13 @@ const Hero = () => {
     useGSAP(()=>{
         gsap.to('#hero',{
             opacity:1,
-            delay:1,
+            delay:2,
+        })
+        gsap.to('#cta',{
+            opacity:1,
+            delay:2,
+            y: -50,
+            ease:"power3.out"
         })
     },[])
 
@@ -48,6 +54,14 @@ const Hero = () => {
                         <source src={videoSrc} type="video/mp4"/>
                     </video>
                 </div>
+            </div>
+            <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+                <a href="#highlights" className="btn">
+                    Buy
+                </a>
+                <p className="font-normal text-xl">
+                    From $199/month or $999
+                </p>
             </div>
         </section>
     )
